a82c4cd59c80c5ddebff17645481f6e775b26a26
Akira Enomoto <ak2monotoe@gmail.com>
2020-07-07 21:25:16 +0900

    finish testpart

---
+++ tests/Feature/ArticleControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ArticleControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    public function testIndex()
+    {
+        $response = $this->get(route('articles.index'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('articles.index');
+    }
+
+    //未ログイン状態であれば、ログイン画面にリダイレクトされること
+    public function testGuestCreate()
+    {
+        $response = $this->get(route('articles.create'));
+
+        $response->assertRedirect(route('login'));
+    }
+    
+    //ログイン済み状態であれば、記事投稿画面が表示されること
+    public function testAuthCreate()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->get(route('articles.create'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('articles.create');
+    }
+}
